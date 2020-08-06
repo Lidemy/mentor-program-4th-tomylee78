8c60ee104459d8f89f05e0dec26484192894aeb1
tomylee78 <tomylee78@gmail.com>
2020-08-06 22:19:47 +0800

    Update hw3.js
    不小心把老師的範例版本跟我的作業版本貼錯在一起了Orz

--- examples/week4/hw3.js
+++ examples/week4/hw3.js
@@ -1,23 +1,27 @@
-// eslint-disable-next-line
-const request = require('request');// node.js內建模擬Client端發出HTTP request的函式庫，可以做簡單的客戶端模擬請求
-const process = require('process');//用來處理CLI使用者輸入的資料
+const request = require('request');
+const args = process.argv;
+const API_ENDPOINT = 'https://restcountries.eu/rest/v2';
 
+const name = args[2];
 
-request(`https://restcountries.eu/rest/v2/name/${process.argv[2]}`, (error, response, body) => { // 發送伺服器規範的HTTP API GET請求
-    const json = JSON.parse(body);
-    const num = json.length;//查詢到幾筆國家
+if (!name) {
+  return console.log('請輸入國家名稱');
+}
 
-    if (response.statusCode !== 404) { //當找到資料時
-        for (let i = 0; i < num; i += 1) {
-            const tt = JSON.stringify(json[i].currencies[0].code);
-            console.log('============');
-            console.log(`國家：${json[i].name}`);
-            console.log(`首都：${json[i].capital}`);
-            console.log(`貨幣：${json[i].currencies[0].code}`);
-            console.log(`國碼：${json[i].callingCodes}`);
-        }
-    } else {
-        console.log('找不到國家資訊');
-    }
-});
+request(`${API_ENDPOINT}/name/${name}`, (err, res, body) => {
+  if (err) {
+    return console.log('抓取失敗', err);
+  }
+  const data = JSON.parse(body);
+  if (data.status === 404) {
+    return console.log('找不到國家資訊')
+  }
 
+  for (let i = 0; i < data.length; i++) {
+    console.log('============')
+    console.log('國家：' + data[i].name);
+    console.log('首都：' + data[i].capital);
+    console.log('貨幣：' + data[i].currencies[0].code);
+    console.log('國碼：' + data[i].callingCodes[0]);
+  }
+}) 
